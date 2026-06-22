11692654455931ed83eecf65ebc9cddb2cba56ac
Marazzano <mdcolon@uw.edu>
2026-06-21 23:05:35 -0700

    Revert "segmentation masks: add RLE utility"
    This reverts commit 3928eb79d1a5991436d6faca90cf4311e38e91dc.

--- src/data_pipeline/segmentation/masks/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Binary mask utilities for segmentation contracts."""
-
-from .mask_rle import decode_binary_mask_rle, encode_binary_mask_rle, validate_binary_mask
-
-__all__ = [
-    "decode_binary_mask_rle",
-    "encode_binary_mask_rle",
-    "validate_binary_mask",
-]
