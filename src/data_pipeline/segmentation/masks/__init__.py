e0fc74704012fb64ba053c2e8bb91d14a2903c10
Marazzano <mdcolon@uw.edu>
2026-06-21 22:52:06 -0700

    segmentation masks: add geometry utility

--- src/data_pipeline/segmentation/masks/__init__.py
+++ src/data_pipeline/segmentation/masks/__init__.py
@@ -1,9 +1,19 @@
 """Binary mask utilities for segmentation contracts."""
 
+from .mask_geometry import (
+    mask_area_px,
+    mask_bounding_box_xyxy_px,
+    mask_centroid_xy_px,
+    mask_geometry,
+)
 from .mask_rle import decode_binary_mask_rle, encode_binary_mask_rle, validate_binary_mask
 
 __all__ = [
     "decode_binary_mask_rle",
     "encode_binary_mask_rle",
+    "mask_area_px",
+    "mask_bounding_box_xyxy_px",
+    "mask_centroid_xy_px",
+    "mask_geometry",
     "validate_binary_mask",
 ]
